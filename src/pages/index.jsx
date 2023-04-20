ff9da7013d73e5b1d336f346ccbbf5f5672207df
Valentin Squirelo <valentin+github@hackerloop.com>
2023-04-20 10:30:24 +0200

    Initial commit

---
+++ src/pages/index.jsx
@@ -0,0 +1,34 @@
+import Head from 'next/head'
+
+import { CallToAction } from '@/components/CallToAction'
+import { Faqs } from '@/components/Faqs'
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+import { Hero } from '@/components/Hero'
+
+import { PrimaryFeatures } from '@/components/PrimaryFeatures'
+import { SecondaryFeatures } from '@/components/SecondaryFeatures'
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>playAbility - Make gaming accessible to everyone</title>
+        <meta
+          name="description"
+          content="Make gaming accessible to everyone"
+        />
+      </Head>
+      <Header />
+      <main>
+        <Hero />
+        <PrimaryFeatures />
+        <SecondaryFeatures />
+        <CallToAction />
+        <Faqs />
+      </main>
+      <Footer />
+    </>
+  )
+}
---
+++ src/pages/simulator.jsx
@@ -0,0 +1,24 @@
+import Head from 'next/head'
+
+import { Footer } from '@/components/Footer'
+import { Header } from '@/components/Header'
+
+
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>TaxPal - Accounting made simple for small businesses</title>
+        <meta
+          name="description"
+          content="Most bookkeeping software is accurate, but hard to use. We make the opposite trade-off, and hope you don’t get audited."
+        />
+      </Head>
+      <Header />
+      <main>
+      </main>
+      <Footer />
+    </>
+  )
+}
