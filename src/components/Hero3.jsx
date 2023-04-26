f8cdf5adf716a7a683249007d856ed9f946d7184
Valentin Squirelo <valentin+github@hackerloop.com>
2023-04-26 13:40:55 +0200

    website only

--- src/components/Hero3.jsx
+++ src/components/Hero3.jsx
@@ -37,7 +37,7 @@ export default function Hero3() {
           <div className="lg:pr-8 lg:pt-3">
             <div className="lg:max-w-lg">
               {/* <h2 className="text-base font-semibold leading-7 text-indigo-600">Deploy faster</h2> */}
-              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-3xl">Jouer à Mario Kart avec votre fauteuil roulant et votre voix ?</p>
+              <p className="mt-2 text-3xl font-bold tracking-tight text-gray-900 sm:text-3xl">Jouer à Mario Kart avec votre fauteuil roulant et votre voix &rsquo;?</p>
               <p className="mt-6 text-lg leading-8 text-gray-600">
               Avec playAbility, c'est possible.
               </p>
--- src/components/Testimonials.jsx
+++ src/components/Testimonials.jsx
@@ -1,6 +1,7 @@
    
 import { useState } from 'react'
 import { Dialog } from '@headlessui/react'
+import Image from 'next/image'
 
 export default function Testimonials() {
 
@@ -21,7 +22,7 @@ export default function Testimonials() {
    <div className="mx-auto flex max-w-7xl flex-col items-center gap-x-8 gap-y-10 px-6 sm:gap-y-8 lg:px-8 xl:flex-row xl:items-stretch">
      <div className="-mt-8 w-full max-w-2xl xl:-mb-8 xl:w-96 xl:flex-none">
        <div className="relative aspect-[2/1] h-full md:-mx-8 xl:mx-0 xl:aspect-auto">
-         <img
+       <Image
            className="absolute inset-0 h-full w-full rounded-2xl bg-gray-800 object-cover shadow-2xl"
            src="https://images.unsplash.com/photo-1517841905240-472988babdf9?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2102&q=80"
            alt=""
