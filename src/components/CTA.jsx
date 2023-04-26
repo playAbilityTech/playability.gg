105eb820189914b95c1e69f0c1176a8693623989
Valentin Squirelo <valentin+github@hackerloop.com>
2023-04-26 15:00:14 +0200

    fix stuff

--- src/components/CTA.jsx
+++ src/components/CTA.jsx
@@ -4,13 +4,11 @@ export default function CTA() {
           <div className="px-6 py-24 sm:px-6 sm:py-32 lg:px-8">
             <div className="mx-auto max-w-2xl text-center">
               <h2 className="text-3xl font-bold tracking-tight text-white sm:text-4xl">
-                Boost your productivity.
-                <br />
-                Start using our app today.
+                Tu veux l'utiliser ou soutenir le projet ? 
+              
               </h2>
               <p className="mx-auto mt-6 max-w-xl text-lg leading-8 text-gray-300">
-                Incididunt sint fugiat pariatur cupidatat consectetur sit cillum anim id veniam aliqua proident excepteur
-                commodo do ea.
+                On lance en juin sur Kickstarter pour une livraison en octobre cette année !
               </p>
               <div className="mt-10 flex items-center justify-center gap-x-6">
                 <a
