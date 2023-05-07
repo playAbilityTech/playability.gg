a69b1ff66e76617e025bff6f7eeda67149023e21
Valentin Squirelo <valentin+github@hackerloop.com>
2023-05-07 15:30:33 +0200

    yt

--- src/components/youtube.jsx
+++ src/components/youtube.jsx
@@ -16,7 +16,7 @@ export default function Youtube() {
                   href="https://www.youtube.com/watch?v=E2yJn7mEN5s"
                   className="rounded-md bg-red-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-red-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-red-600"
                 >
-                  Regardez la video sur youtube  <span aria-hidden="true">&rarr;</span>
+                  Regardez la video  <span aria-hidden="true">&rarr;</span>
                 </a>
               </div>
             </div>
